a186638128d586e9044c196059b026ffc60b8c95
Mohammad s. Albay <43953576+MohammadAlbay@users.noreply.github.com>
2024-06-29 00:37:15 +0200

    Fixed migration 3. added seeder

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table("departments")->insert([
+            'department_id' => "WT",
+            'name' => "Web Technology"
+        ]);
+    }
+}
